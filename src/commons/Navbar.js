a88a564ff1492bce1bd4e6087ba68a8b7f4c6527
Luis Casillas <luiscasillas@n12.mx>
2019-04-04 20:09:17 -0600

    Route added

---
+++ src/commons/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Navbar =()=>{
+  return(
+    <section>
+      <h2>
+        Esto es la navbar
+      </h2>
+    </section>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import './index.css';
 import App from './App';
+import {Login} from './components'
+import {Navbar} from './commons'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+      <main className="App">
+        <Navbar/>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/login" component={Login}/>
+          </Switch>
+        </Router>
+      </main>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
