6219807e2d951c5e144feca69c84eadc3f64f3c2
Luis Casillas <luiscasillas@n12.mx>
2019-04-04 20:28:04 -0600

    base structure navbar

--- src/commons/Navbar.js
+++ src/commons/Navbar.js
@@ -2,17 +2,40 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-export const Navbar =()=>{
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+export const Navbar =(props)=>{
+  const {classes={}} = props
   return(
     <section>
-      <AppBar position="static" color="default">
-       <Toolbar>
-         <Typography variant="h6" color="inherit">
-           Photos
-         </Typography>
-       </Toolbar>
-     </AppBar>
+      <div>
+       <AppBar position="static">
+         <Toolbar>
+           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+             <MenuIcon />
+           </IconButton>
+           <Typography variant="h6" color="inherit" className={classes.grow}>
+             News
+           </Typography>
+           <Button color="inherit">Login</Button>
+         </Toolbar>
+       </AppBar>
+     </div>
     </section>
   )
 }
